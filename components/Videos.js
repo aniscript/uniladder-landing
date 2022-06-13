49ff1ed67f27d18add45651587e18d1cd046af0f
Anish Lamsal <aneeslml@gmail.com>
2022-06-13 16:51:45 +0100

    Video

--- components/Videos.js
+++ components/Videos.js
@@ -22,7 +22,7 @@ const Videos = () => {
             <iframe
               width="100%"
               height="250"
-              src="https://www.youtube.com/embed/Cg-oRAm6QBk"
+              src="https://www.youtube.com/embed/MZha-oIlmPs"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
