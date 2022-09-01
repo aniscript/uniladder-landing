5f087ea972269f0d5a0256bf1dd9fd31d09ebc18
aneeslml <aneeslml@gmail.com>
2022-09-01 11:31:54 +0100

    Sample Documents

---
+++ components/sample-list.js
@@ -0,0 +1,95 @@
+const sampleList = [
+  {
+    id: 1,
+    link: "/assets/documents/cv.docx",
+    title: "Curriculum Vitae",
+    description:
+      "Our team guides the student to choose the best suited courses and university for your career.",
+  },
+  {
+    id: 2,
+    link: "/assets/documents/sop.pdf",
+    title: "Statement of Purpose",
+    description:
+      "We are affiliated with the top universities in the UK. We are able to process the admissions faster and more efficiently.",
+  },
+  {
+    id: 3,
+    link: "/assets/documents/recommendation.pdf",
+    title: "Recommendation from Work",
+    description:
+      "We have 100% visa success rate in last academic year. We provide best consultation service regarding the visa procedure.",
+  },
+  {
+    id: 4,
+    link: "/assets/documents/experience_letter.docx",
+    title: "Work Experience",
+    description:
+      "An experience certificate is a letter that is written on behalf of an employee. It helps confirm the time the employee was associated with the company and attests his skills, intelligence and work habits. The experience certificate is one of the very important documents that helps secure a new job or bridge the education GAP. An experience certificate should be just about one paragraph long and not an entire page long like the reference letter format.",
+  },
+  {
+    id: 5,
+    link: "/assets/documents/balance.pdf",
+    title: "Bank Statement",
+    description:
+      "We have dedicated team to provide the best accomodation for our students in the UK, so students can directly go to their accomodation after arrival. ",
+  },
+  {
+    id: 6,
+    link: "/assets/documents/loan.pdf",
+    title: "Education Loan",
+    description:
+      "We have dedicated team to provide the best accomodation for our students in the UK, so students can directly go to their accomodation after arrival. ",
+  },
+  {
+    id: 7,
+    link: "/assets/documents/affidavit_child.docx",
+    title: "Affidavit letter for Child",
+    description:
+      "We provide guidance to the students after their arrival regarding different registrations(such as GP, NI, etc.) and job finding in the UK ",
+  },
+  {
+    id: 8,
+    link: "/assets/documents/affidavit_husband.docx",
+    title: "Affidavit letter for Spouse",
+    description:
+      "We provide guidance to the students after their arrival regarding different registrations(such as GP, NI, etc.) and job finding in the UK ",
+  },
+  {
+    id: 9,
+    link: "/assets/documents/cover_visa.docx",
+    title: "Cover letter Dependent Visa",
+    description:
+      "We provide guidance to the students after their arrival regarding different registrations(such as GP, NI, etc.) and job finding in the UK ",
+  },
+  {
+    id: 10,
+    link: "/assets/documents/marriage.pdf",
+    title: "Marriage Certificate",
+    description:
+      "We provide guidance to the students after their arrival regarding different registrations(such as GP, NI, etc.) and job finding in the UK ",
+  },
+  {
+    id: 11,
+    link: "/assets/documents/relationship.pdf",
+    title: "Relationship Certificate",
+    description:
+      "We provide guidance to the students after their arrival regarding different registrations(such as GP, NI, etc.) and job finding in the UK ",
+  },
+  {
+    id: 12,
+    link: "/assets/documents/checklist.pdf",
+    title: "Checklist",
+    description:
+      "We provide guidance to the students after their arrival regarding different registrations(such as GP, NI, etc.) and job finding in the UK ",
+  },
+  {
+    id: 13,
+    link: "/assets/documents/process.png",
+    title: "Application Process",
+    description:
+      "We provide guidance to the students after their arrival regarding different registrations(such as GP, NI, etc.) and job finding in the UK ",
+  },
+];
+
+export default sampleList;
